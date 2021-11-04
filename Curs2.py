2e12227157d44769edb74bbf1b18b34dc8e6c689
Puiu-Sebastian-Andrei <scorpionsebi@yahoo.com>
2021-11-04 19:37:38 +0200

    Adaugare lucrare

---
+++ Curs2.py
@@ -0,0 +1,9 @@
+
+dictionar = {"key1" : "value1", "key2" : "value2"}
+for item in dictionar.values():
+    print(item)
+
+variabila = ('1','2')
+print(type(variabila))
+variabila = "Ana \"are\" mere"
+print(variabila)
